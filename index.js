d291b3e79c2d37dc6810afa21ebc729ca0ff6fcf
Julien Despois <despois.j@gmail.com>
2023-08-03 13:02:44 +0200

    Add toggle see more / less

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
 function replaceDigitsWithCircledNumbers(text) {
     // Unicode offset for circled numbers: 9311 (0x2460)
     const unicodeOffset = 9311;
@@ -17,4 +33,23 @@ $(document).ready(function() {
     const convertedText = replaceDigitsWithCircledNumbers(originalText);
     $(this).html(convertedText);
   });
+
+  // Display on click
+  $(".seeMore").click(function(event) {
+    $(this).parent().find(".elementContents").slideToggle(700);
+
+    // Buttons
+    $(this).hide();
+    $(this).parent().find(".seeLessContainer").show();
+  });
+
+  $(".seeLess").click(function(event) {
+    $(this).parent().parent().find(".elementContents").slideToggle(400);
+
+    // Buttons
+    $(this).parent().hide();
+    $(this).parent().parent().find(".seeMore").show();
+  });
+
+
 });
--- tools.php
+++ tools.php
@@ -13,40 +13,59 @@ function element($pageName, $title, $styles, $composers, $type) {
 }
 
 
-function loadElement($element) {
-	echo "<div class='element'>";
-
-	// Setup title link
-	echo '<a style="color:white; text-decoration:none" href="index.php?filter=element&element=' . $element["pageName"] . '"><h2 class="elementTitle">' . $element["title"] . "</h4></a>";
-
-	include "elements/".$element["pageName"].".html";
-
-	// Tags
-	echo "<div class='tagBox'>";
-	foreach ($element["styles"] as $style) {
-		echo "<i><a class='tag' href='index.php?filter=style&style=" . $style . "'>" . ucfirst($style) . "</a></i>, ";
-	}
-	foreach ($element["composers"] as $composer) {
-		echo "<i><a class='tag' href='composers.php?filter=composer&composer=" . $composer . "'>" . ucfirst($composer) . "</a></i>, ";
-	}
-	echo "</div>";
-
-	echo "</div>";
-}
-
 // Define elements
 $mapping = [];
-array_push($mapping, element("chopin_chord", "'Chopin Chord' (V7Add6)", ["romantic"], ["chopin"], "harmony"));
+array_push($mapping, element("chopin_chord", "'Chopin Chord' (V7Add6)", ["romantic"], ["chopin", "scriabin"], "harmony"));
 array_push($mapping, element("V9_chord", "V9 Chord", ["romantic"], ["chopin"], "harmony"));
 array_push($mapping, element("moonlight_modulation", "'Moonlight Modulation'", ["romantic"], ["beethoven"], "harmony"));
 array_push($mapping, element("augmented_chord", "Augmented Chords", ["romantic"], ["rachmaninoff"], "harmony"));
 array_push($mapping, element("lh_arps", "Left Hand Broken Arpeggios", ["romantic"], ["chopin", "scriabin"], "texture"));
 
 
+function loadElement($element, $display) {
+	// Loads a singular element
+
+	echo "<div class='element'>";
+
+		// Setup title link
+		echo '<h2 class="elementTitle"><a style="color:white; text-decoration:none" href="index.php?filter=element&element=' . $element["pageName"] . '">' . $element["title"] . "</a></h4>";
+
+
+		// If togglable add see more / less
+		if (!$display)
+			echo '<a class="seeMore">Show</a>';
+
+		// Possibility to collapse or not by default
+		if ($display)
+			echo '<div class="elementContents">';
+		else
+			echo '<div class="elementContents" style="display: none">';
+
+		include "elements/".$element["pageName"].".html";
+
+		echo '</div>';
+
+		// If togglable add see more / less
+		if (!$display)
+			echo '<div class="seeLessContainer" style="display:none;"><a class="seeLess">Hide</a></div>';
+
+		// Tags
+		echo "<div class='tagBox'>";
+			foreach ($element["styles"] as $style) {
+				echo "<i><a class='tag' href='index.php?filter=style&style=" . $style . "'>" . ucfirst($style) . "</a></i>, ";
+			}
+			foreach ($element["composers"] as $composer) {
+				echo "<i><a class='tag' href='index.php?filter=composer&composer=" . $composer . "'>" . ucfirst($composer) . "</a></i>, ";
+			}
+		echo "</div>";
+
+	echo "</div>";
+}
+
 function loadAllElements() {
 	global $mapping;
 	foreach ($mapping as $element) {
-		loadElement($element);
+		loadElement($element, False);
 	}
 }
 
@@ -55,7 +74,7 @@ function loadStyle($style) {
 	foreach ($mapping as $element) {
 		foreach ($element["styles"] as $currStyle){
 			if ($currStyle == $style){
-				loadElement($element);
+				loadElement($element, False);
 			}
 		}
 	}
@@ -66,7 +85,7 @@ function loadComposer($composer) {
 	foreach ($mapping as $element) {
 		foreach ($element["composers"] as $currComposer){
 			if ($currComposer == $composer){
-				loadElement($element);
+				loadElement($element, False);
 			}
 		}
 	}
@@ -76,7 +95,7 @@ function loadType($type) {
 	global $mapping;
 	foreach ($mapping as $element) {
 		if ($element["type"] == $type){
-			loadElement($element);
+			loadElement($element, False);
 		}
 	}
 }
@@ -85,7 +104,7 @@ function loadSingleElement($elementName) {
 	global $mapping;
 	foreach ($mapping as $element) {
 		if ($element["pageName"] == $elementName){
-			loadElement($element);
+			loadElement($element, True); // Uncollapse
 		}
 	}
 }
