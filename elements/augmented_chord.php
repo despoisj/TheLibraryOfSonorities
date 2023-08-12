2f37fee199d621b8dc4b8f48ca868a67f83a63db
Julien Despois <despois.j@gmail.com>
2023-08-12 11:29:28 +0200

    aug

--- elements/augmented_chord.php
+++ elements/augmented_chord.php
@@ -1,9 +1,13 @@
-<p>Can be used as passing chords.</p>
+<p>Augmented triads are often used as passing chords.</p>
 
 <h4 class="elementExampleTitle">Examples</h4>
 <p>5/3 -> 5+/3 -> 6</p>
 
 <!-- TODO CENTERING -->
 <?php yt("HocZnbMrC24", 39) ?>
+
+<p>• First bar of 2nd line: I -> I6/3+ -> IV</p>
+<p>• First bar of 4th line: V -> V5+/3 -> I</p>
+<?php yt("vEGxyHe4Azo") ?>
 
 <iframe src="https://flat.io/embed/64ca55598651f784731dce8a?_l=true&sharingKey=d551126e08404ac7775c39e3d4339f159968fe203e8095800d07c2c842766be745711790d82b164c0194239eae55066782b0c9a24688a83fb47d3671d15faa60" height="315" width="560" frameBorder="0" allowfullscreen allow="autoplay; midi"></iframe>
