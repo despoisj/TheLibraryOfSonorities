ea9d38b2184cee9fc7fb188fb4b1eda05146d759
Julien Despois <despois.j@gmail.com>
2023-08-16 17:12:53 +0200

    Add

--- elements/chopin_chord.php
+++ elements/chopin_chord.php
@@ -1,24 +1,28 @@
-<p>The Chopin Chord is a Dominant 7th chord with the added 13th (also called 6th) sometimes voiced in 4ths with ^4 ^7 ^3 in the soprano, or ^4 ^7 b^3 in minor.</p>
+<p>The Chopin Chord is a Dominant 7th chord with the added 13th (also called 6th) sometimes voiced in 4ths with ^4 ^7 ^3 in the soprano, or ^4 ^7 b^3 in minor. Most often, the 13th resolves down to the 5th (^3 -> ^2) and then the chord resolves like a normal dominant 7th chord. The 6th/13th is an unprepared suspension.</p>
 <img style="width:90%; background-color: white;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Dominant_chords_in_common_practice_period.png/1000px-Dominant_chords_in_common_practice_period.png">
 
 <h4 class="elementExampleTitle">Examples</h4>
-<p>Measures 2. and 4.</p>
+<p>• Measures 2. and 4.</p>
 
 <?php yt("B_Qa9uVwhhU") ?>
 
-<p style="margin-top: 30px;">3rd beat of measure 1, 2 and 4</p>
+<p style="margin-top: 30px;">• 3rd beat of measure 1, 2 and 4</p>
 <?php yt("XeX4X_1_lo0") ?>
 
-<p style="margin-top: 30px;">2nd measure</p>
+<p>• Schubert: 2nd row, I64 (B G# E) -> B-D#-x-A + G# (13th/6th) -> I</p>
+<?php yt("Vy_HeH-pRfI", 611) ?>
+
+<p style="margin-top: 30px;">• 2nd measure</p>
 <?php yt("eOS0RJ15Wt8", 20) ?>
 
-<p style="margin-top: 30px;">3rd measure: Eb7: Eb-G-Bb-Db + C (13th/6th)</p>
+<p style="margin-top: 30px;">• 3rd measure: Eb7: Eb-G-Bb-Db + C (13th/6th)</p>
 <?php yt("xD5TKaa3lR8", 30) ?>
-<?php yt("-uwdGzclfi4") ?>
 
-
-<p style="margin-top: 30px;">2nd measure: E7 + C#(13th/6th)</p>
+<p style="margin-top: 30px;">• 2nd measure: E7 + C#(13th/6th)</p>
 <?php yt("3QH8MstNkKg", 302) ?>
+
+<!-- Ref -->
+<?php yt("-uwdGzclfi4") ?>
 
 
 <iframe src="https://flat.io/embed/64ca10eb8651f7847317793d?_l=true&sharingKey=0ef4f94f608c46d92b82a15460de7ceceaa799a66347f55087ab413dc60af416655444e0f92fbdea32e7b4378c0ba1379dc73e1032ade12f6f19a574b8f61548" height="315" width="560" frameBorder="0" allowfullscreen allow="autoplay; midi"></iframe>
