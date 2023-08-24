acdd031702768492fff11bd3ae74ca3b942caac8
Julien Despois <despois.j@gmail.com>
2023-08-24 21:51:53 +0200

    Add moonlight modulation

--- elements/moonlight_modulation.php
+++ elements/moonlight_modulation.php
@@ -10,5 +10,9 @@
 <p>Bottom row, 2nd -> 3rd measure.</p>
 <?php yt("Fy3JxUfE9ds", 35) ?>
 
+<?php tt("Vasily Kalinnikov - Waltz in A")?>
+<p>mm. 2: C#min -> B-D-(E)-G# -> Amaj</p>
+<?php yt("oLfvyClF0ZU", 26) ?>
+
 <?php tt("Model")?>
 <iframe src="https://flat.io/embed/64ca3fdeaeee0065c425b807?_l=true&sharingKey=3f4b36dcce4c5fc8b7d8c32c195eda39850450ea3c336672d43b0b187f4135c24bc90957b49ebc0edd77774384fea771f3243bcf4fcb28bc9d4c157155811ad0" height="315" width="560" frameBorder="0" allowfullscreen allow="autoplay; midi"></iframe>
