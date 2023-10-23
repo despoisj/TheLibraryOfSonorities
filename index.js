8ae510a08205b1e2046cf41c1011f3050986f113
Julien Despois <despois.j@gmail.com>
2023-10-23 21:55:02 +0200

    Fix circles

--- index.js
+++ index.js
@@ -27,18 +27,14 @@ function replaceDigitsWithCircledNumbers(text) {
     // Unicode offset for circled numbers: 9311 or 10111 (0x2460)
 
     // Replace matched digits with their corresponding circled numbers
-    // text = text.replace(regex, (_, digit) => "<span style='font-size:20px;'>" + String.fromCharCode(parseInt(digit) + unicodeOffset) + "</span>");
-    text = text.replace(regex, (_, digit) => "<span>" + String.fromCharCode(parseInt(digit) + unicodeOffset) + "</span>");
-
-
+    text = text.replace(regex, (_, digit) => String.fromCharCode(parseInt(digit) + unicodeOffset));
 
     // Regular expression to match digits with a star symbol (*)
     const regexBass = /\*(\d)/g;
     const unicodeOffsetBass = 10111; // For back background: soprano
 
     // Replace matched digits with their corresponding circled numbers
-    // text = text.replace(regexBass, (_, digit) => "<span style='font-size:20px;'>" + String.fromCharCode(parseInt(digit) + unicodeOffsetBass) + "</span>");
-    text = text.replace(regexBass, (_, digit) => "<span>" + String.fromCharCode(parseInt(digit) + unicodeOffsetBass) + "</span>");
+    text = text.replace(regexBass, (_, digit) => String.fromCharCode(parseInt(digit) + unicodeOffsetBass));
 
 
 
