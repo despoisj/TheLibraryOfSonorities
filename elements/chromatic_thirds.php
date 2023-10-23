be38fa876c993a877ca3d96580c5b3df2ecfd421
Julien Despois <despois.j@gmail.com>
2023-10-23 14:42:48 +0200

    Added chromatic third sixths

--- elements/chromatic_thirds.php
+++ elements/chromatic_thirds.php
@@ -1,4 +1,21 @@
-<p>Use of thirds for the melody, especially with retardations / chromatic approach tones.</p>
+<p>Use of thirds or sixths for the melody, especially with retardations / chromatic approach tones.</p>
 
 <h4 class="elementExampleTitle">Examples</h4>
+<?php tt("Salomon Mazurette - Nocturne, Op. 24") ?>
 <?php yt("xD5TKaa3lR8", 11) ?>
+
+<?php tt("Chopin - Nocturnes") ?>
+<p>- 3:26 (Nocturne n°1), 35:01 (Nocturne n°7), 36:33 (Nocturne n°8), 43:30 (Nocturne n°9), 59:11 (Nocturne n°12), probably more.</p>
+<?php yt("wuL7UC2glJM", 206) ?>
+
+
+<?php tt("John Field - Nocturnes") ?>
+<p>- 34:33 (Nocturne n°8), 1:07:33 (Nocturne n°16), probably more.</p>
+<?php yt("U-fS3mR9KDA", 2073) ?>
+
+<?php tt("Chopin - Grande Polonaise Brillante, Op. 22") ?>
+<?php yt("jJ130s7BiSM", 539) ?>
+
+<?php tt("Chopin - Waltzes") ?>
+<p>- 7:01 (Waltz n°2), 33:56 (Waltz n°9), 39:10 (Waltz n°11), 41:40 (Waltz n°12), etc.</p>
+<?php yt("tiTu6g2TqC8", 421) ?>
