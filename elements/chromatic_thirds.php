68d9149ae0753f6445dc2b158ebcc7a5b5bbd7e4
Julien Despois <despois.j@gmail.com>
2023-10-23 15:02:09 +0200

    Add sixths prelude rach

--- elements/chromatic_thirds.php
+++ elements/chromatic_thirds.php
@@ -1,8 +1,9 @@
-<p>Use of thirds or sixths for the melody, especially with retardations / chromatic approach tones.</p>
+<p>The use of thirds or sixths for the melody, especially with retardations / chromatic approach tones, gives a very rich and sophisticated romantic sound.</p>
 
 <h4 class="elementExampleTitle">Examples</h4>
 <?php tt("Salomon Mazurette - Nocturne, Op. 24") ?>
 <?php yt("xD5TKaa3lR8", 11) ?>
+
 
 <?php tt("Chopin - Nocturnes") ?>
 <p>- 3:26 (Nocturne n°1), 35:01 (Nocturne n°7), 36:33 (Nocturne n°8), 43:30 (Nocturne n°9), 59:11 (Nocturne n°12), probably more.</p>
@@ -13,8 +14,16 @@
 <p>- 34:33 (Nocturne n°8), 1:07:33 (Nocturne n°16), probably more.</p>
 <?php yt("U-fS3mR9KDA", 2073) ?>
 
+<?php tt("Rachmaninoff - Elegie, Op. 3 n°1") ?>
+<p>Thirds at 3:01, sixths at 4:19.</p>
+<?php yt("OipkSouZPhyk", 181) ?>
+
 <?php tt("Chopin - Grande Polonaise Brillante, Op. 22") ?>
 <?php yt("jJ130s7BiSM", 539) ?>
+
+<?php tt("Rachmaninoff - Prelude, Op. 23 n°4") ?>
+<p>Blend of thirds & sixths.</p>
+<?php yt("1qoPDQzXVmk", 167) ?>
 
 <?php tt("Chopin - Waltzes") ?>
 <p>- 7:01 (Waltz n°2), 33:56 (Waltz n°9), 39:10 (Waltz n°11), 41:40 (Waltz n°12), etc.</p>
