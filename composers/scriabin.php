be3bd1ccefdb9a88c9d2fb8bb6d0b3cdfcb5a224
Julien Despois <despois.j@gmail.com>
2024-01-31 07:25:36 +0100

    Add scriabin blurb

---
+++ composers/scriabin.php
@@ -0,0 +1,2 @@
+<p>Heavily influenced by Chopin before finding his own characteristic voice, Scriabin is known for his lenghty phrases - often starting off-beat, intricated melodies and harmonies using extended chords. Scriabin also seemed to love uneven note groupings (triplets, septuplets, etc.) including against different LH groupings to create complex polyrythms.<p>
+<p>Note: the material on this website only covers Scriabin's 'First Period' up to ~1905.</p>
