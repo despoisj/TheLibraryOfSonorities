49d7698d334d086a1ecd01c88bc4526837bef6a1
Julien Despois <despois.j@gmail.com>
2024-04-28 19:32:01 +0200

    Add baroque to picardy thirs and matthieu's exampels

--- elements/picardy_third.php
+++ elements/picardy_third.php
@@ -1,4 +1,4 @@
-<p>The <a href="https://en.wikipedia.org/wiki/Picardy_third">picardy third</a> is the name given to resolving a minor harmony to major, often at the end the piece or a section. In Romantic music, the change is often sudden, and gives a very hopeful feeling, like the sun peeking out of the clouds after a storm. </p>
+<p>The <a href="https://en.wikipedia.org/wiki/Picardy_third">picardy third</a> is the name given to resolving a minor harmony to major, often at the end the piece or a section. The change is often sudden, and gives a very hopeful feeling, like the sun peeking out of the clouds after a storm. </p>
 
 <p style="text-align: center;"><a target="_blank" href="https://en.wikipedia.org/wiki/Neapolitan_chord">Wikipedia</a></p>
 
@@ -9,9 +9,17 @@
 <p>Note: many of Chopin's minor nocturnes end this way.</p>
 <?php yt("tVV3SIvncD4", 195) ?>
 
+<?php tt("Bach - Little Fugue in G minor, BWV 578")?>
+<p>G minor -> G major.</p>
+<?php yt("Bbox4oi6HjA", 199) ?>
+
 <?php tt("Chopin - Fantasie-Impromptu")?>
 <p>After a tumultuous piece in C# minor, the ending is in C# major.</p>
 <?php yt("75x6DncZDgI", 270) ?>
+
+<?php tt("Bach - Prelude n°2 WTC Book 1")?>
+<p>C minor -> C major.</p>
+<?php yt("HB8-w5CvMls", 92) ?>
 
 <?php tt("Fauré - Requiem, Libera Me")?>
 <p>We first get A minor (mm. 4), then A major.</p>
@@ -20,6 +28,12 @@
 <?php tt("Chopin - Etude Op. 10 n°6")?>
 <p>Eb minor -> Eb Major.</p>
 <?php yt("kY768DAyTuk", 236) ?>
+
+
+<?php tt("Bach - French Suite n°1, 'Sarabande' BWV 812")?>
+<p>D minor -> D major.</p>
+<?php yt("nabd9V8C3MY", 440) ?>
+
 
 <?php tt("Samuel Maykapar - Arietta in A minor")?>
 <p>- At 2:10, the first measure of the 2nd row gives an A major (A7) sound in a piece where we always had A minor.</p>
