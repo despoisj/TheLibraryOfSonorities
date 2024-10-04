53f4c1326d808f6159bee15c757c6223f4e20380
Julien Despois <despois.j@gmail.com>
2024-10-04 11:52:04 +0200

    Add bach to IV iii

--- elements/ivmaj7_iiimin7.php
+++ elements/ivmaj7_iiimin7.php
@@ -1,10 +1,15 @@
-<p>This move goes from IVM7 to iiim7.</p>
+<p>This move goes from IVM7 to iiim7, with or without the fifth like a 7-6 chain. This is a very rare sonority, but I find it very stiking.</p>
 
 <h4 class="elementExampleTitle">Examples</h4>
+
+<?php tt("Bach - Toccata and Fugue in D minor, ") ?>
+<p>At 4:01, we get twice a 7/5/3 -> 7/6/3 sonority over a *4 -> *3 bassline. The outer voices are in thirds, which is in my opinion essential to the gorgeous sound of this passage.</p>
+<?php yt("SGKfqSJbeAg", 241) ?>
 
 <?php tt("Felix Blumenfeld - Etude Op. 25 n°1") ?>
 <p>This move occurs at the triumphant climax of this section (6:24) bringing, in my opinion, a bitersweet feel.</p>
 <?php yt("GnPL50isfHE", 384) ?>
+
 
 <p style="margin-top: 35px;">Approximate reduction of the same passage</p>
 <?php fl("64cba2b629cc26ec386aac8b?_l=true&sharingKey=525a08cb5160e869230dee567626e081b38c07cc31255815bd694b836d273eed3ce697de12af550b1ba1a1e03e3519661202df98237165e2e7972bafd57aa291") ?>
